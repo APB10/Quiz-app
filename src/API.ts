228bd92e4c0a8b0d7e99b49a577b8fac8e474dcf
Ali <ali_panahi10@hotmail.com>
2022-05-19 11:59:54 +0100

    finished project, functionality to display quiz questions and check user answer with api data and lastly styling added

--- src/API.ts
+++ src/API.ts
@@ -2,14 +2,12 @@ import { shuffleArray } from "./utils";
 
 export type Question = {
   category: string;
-  correct_answers: string;
+  correct_answer: string;
   difficulty: string;
   incorrect_answers: string[];
   question: string;
   type: string;
 };
-
-export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty {
   EASY = "easy",
@@ -17,19 +15,19 @@ export enum Difficulty {
   HARD = "hard",
 }
 
+export type QuestionsState = Question & { answers: string[] };
+
 export const fetchQuizQuestions = async (
   amount: number,
   difficulty: Difficulty
-) => {
+): Promise<QuestionsState[]> => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=15&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
   return data.results.map((question: Question) => ({
     ...question,
     answers: shuffleArray([
       ...question.incorrect_answers,
-      question.correct_answers,
+      question.correct_answer,
     ]),
   }));
 };
-
-export default fetchQuizQuestions;
